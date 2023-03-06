c77e68b9ba09c712b57fce6a51b881021984f3ca
thanhnhan3286 <dothanhnhan362000@gmail.com>
2023-03-06 13:46:27 +0700

    first commit

---
+++ module_2/src/Introduction_to_Java/bai_tap/HienThiLoiChao.java
@@ -0,0 +1,13 @@
+package Introduction_to_Java.bai_tap;
+
+import java.util.Scanner;
+
+public class
+HienThiLoiChao {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter your name: ");
+        String name = sc.nextLine();
+        System.out.println("Hello " + name);
+    }
+}
---
+++ module_2/src/Introduction_to_Java/bai_tap/UngDungDocSoThanhChu.java
@@ -0,0 +1,4 @@
+package Introduction_to_Java.bai_tap;
+
+public class UngDungDocSoThanhChu {
+}
