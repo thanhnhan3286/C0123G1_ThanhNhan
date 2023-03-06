cf7c66360e9049d55c4799d49d1a1e55fceb0f81
thanhnhan3286 <dothanhnhan362000@gmail.com>
2023-03-06 20:52:22 +0700

    first commit

--- module2/src/Introduction_to_Java/bai_tap/UngDungDocSoThanhChu.java
+++ module2/src/Introduction_to_Java/bai_tap/UngDungDocSoThanhChu.java
@@ -5,61 +5,108 @@ import java.util.Scanner;
 public class UngDungDocSoThanhChu {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        while (1 > 0) {
+//        boolean flag;
+         do{
             System.out.print("Nhập số từ 0 -> 999: ");
             int so = Integer.parseInt(sc.nextLine());
-            if (so >= 0) {
-                if (so <= 13) {
-                    switch (so) {
-                        case 0:
-                            System.out.print("Zero");
-                            break;
-                        case 1:
-                            System.out.print("One");
-                            break;
-                        case 2:
-                            System.out.print("Two");
-                            break;
-                        case 3:
-                            System.out.print("Three");
-                            break;
-                        case 4:
-                            System.out.print("Four");
-                            break;
-                        case 5:
-                            System.out.print("Five");
-                            break;
-                        case 6:
-                            System.out.print("Six");
-                            break;
-                        case 7:
-                            System.out.print("Seven");
-                            break;
-                        case 8:
-                            System.out.print("Eight");
-                            break;
-                        case 9:
-                            System.out.print("Nine");
-                            break;
-                        case 10:
-                            System.out.print("Ten");
-                            break;
-                        case 11:
-                            System.out.print("Eleven");
-                            break;
-                        case 12:
-                            System.out.print("Twelve");
-                            break;
-                        case 13:
-                            System.out.print("Thirteen");
-                            break;
+            if (so > 0) {
+                if (so > 19) {
+                    if (so > 100) {
+                        if (so > 1000) {
+                            System.out.print("Số vừa nhập lớn hơn 999"+'\n');
+                        } else {
+                            int a = so / 100;
+                            int b = (so % 100);
+                            int c = (so % 100) % 10;
+                            if (b > 0) {
+                                System.out.print(donVi(a) + " hundered and " + chuc(b) + donVi(c) +'\n');
+                            } else {
+                                System.out.print(donVi(a) + " hundered " + chuc(b) + donVi(c)+'\n');
+                            }
+                        }
+                    } else {
+                        int b = so % 10;
+                        System.out.print(chuc(so) + donVi(b)+'\n');
                     }
-                } else if (so <= 19) {
-
+                } else {
+                    System.out.print(donVi(so)+'\n');
                 }
             } else {
-                System.out.print("Số không hợp lệ");
+                System.out.print("Số không thỏa mãn"+'\n');
             }
+//            flag = true;
+        }while (1>0);
+    }
+
+
+    public static String donVi(int a) {
+        switch (a) {
+            case 1:
+                return "one";
+            case 2:
+                return "two";
+            case 3:
+                return "three";
+            case 4:
+                return "four";
+            case 5:
+                return "five";
+            case 6:
+                return "six";
+            case 7:
+                return "seven";
+            case 8:
+                return "eight";
+            case 9:
+                return "nine";
+            case 10:
+                return "ten";
+            case 11:
+                return "eleven";
+            case 12:
+                return "twelve";
+            case 13:
+                return "thirteen";
+            case 14:
+                return "fourteen";
+            case 15:
+                return "fifteen";
+            case 16:
+                return "sixteen";
+            case 17:
+                return "seventeen";
+            case 18:
+                return "eighteen";
+            case 19:
+                return "nineteen";
+            default:
+                return "";
+        }
+    }
+
+    public static String chuc(int b) {
+        int b1 = b / 10;
+        switch (b1) {
+            case 1:
+                return donVi(b);
+            case 2:
+                return "twenty ";
+            case 3:
+                return "thirty ";
+            case 4:
+                return "forty ";
+            case 5:
+                return "fifty ";
+            case 6:
+                return "sixty ";
+            case 7:
+                return "seventy ";
+            case 8:
+                return "eighty ";
+            case 9:
+                return "ninety ";
+            default:
+                return "";
         }
     }
 }
