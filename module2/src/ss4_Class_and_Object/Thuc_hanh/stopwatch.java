6afd7b491e8c43cfb6efc7e004a5a30694c09b31
thanhnhan3286 <dothanhnhan362000@gmail.com>
2023-03-10 01:15:25 +0700

    first commit

--- module2/src/ss4_Class_and_Object/Thuc_hanh/stopwatch.java
+++ module2/src/ss4_Class_and_Object/Thuc_hanh/stopwatch.java
@@ -4,29 +4,52 @@ import java.util.Scanner;
 
 public class stopwatch {
     long startTime, endTime;
-    public stopwatch(long startTime, long endTime){
-        this.startTime = startTime;
-        this.endTime = endTime;
+
+    public stopwatch() {
     }
-    public long start(){
-        this.startTime = System.currentTimeMillis();
-        return this.startTime;
+
+    private long getStartTime() {
+        return startTime;
     }
-    public long stop(){
-        this.endTime = System.currentTimeMillis();
-        return this.endTime;
+
+    private long getEndTime() {
+        return endTime;
     }
-    public long getElapsedTime(){
-        long elaspedTime = this.endTime - this.startTime;
+
+    public long start() {
+        startTime = System.currentTimeMillis();
+        return startTime;
+    }
+
+    public long stop() {
+        endTime = System.currentTimeMillis();
+        return endTime;
+    }
+
+    public long getElapsedTime() {
+        long elaspedTime = endTime - startTime;
         return elaspedTime;
     }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         stopwatch stopwatch = new stopwatch();
-        System.out.println("Nhập 1 để bắt đầu: ");
-        int c = sc.nextInt();
-        if (c == 1) {
-
-        }
+        int c = 0;
+        do {
+            System.out.println("Nhập 1 để bắt đầu: ");
+            c = sc.nextInt();
+            if (c == 1) {
+                stopwatch.start();
+            }
+        } while (c != 1);
+        int d = 1;
+        do {
+            System.out.println("Nhập 0 để kết thúc: ");
+            d = sc.nextInt();
+            if (d == 0) {
+                stopwatch.stop();
+            }
+        } while (d != 0);
+        System.out.println("Đếm được số milisecond giây: " + stopwatch.getElapsedTime() + "s");
     }
 }
