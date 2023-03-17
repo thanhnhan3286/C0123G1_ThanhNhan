946f4a1746e42299196d506cd5985bc990dab90a
thanhnhan3286 <dothanhnhan362000@gmail.com>
2023-03-17 15:58:18 +0700

    first commit

--- module2/src/ss10_dsa_danh_sach/bai_tap/java_util_array_list/MyList.java
+++ module2/src/ss10_dsa_danh_sach/bai_tap/java_util_array_list/MyList.java
@@ -10,23 +10,91 @@ public class MyList<E> {
     public MyList() {
         elements = (E[]) new Object[DEFAULT_CAPACITY];
     }
-    public MyList(int capacity) {
-        elements = (E[]) new Object[capacity];
+
+    public MyList(int size) {
+        elements = (E[]) new Object[size];
     }
-    private void ensureCapa(){
-        int newSize = elements.length*2;
-        elements = Arrays.copyOf(elements, newSize);
+
+    private void ensureCapa() {
+        if (size > elements.length) {
+            int newSize = size * 2;
+            elements = Arrays.copyOf(elements, newSize);
+        }
     }
-    public void add(E e){
-        if (size == elements.length){
+
+    public void add(E e) {
+        size += 1;
+        ensureCapa();
+        elements[size - 1] = e;
+    }
+
+    public boolean add(E e, int index) {
+        if (index >= 0 && index <= size) {
+            size += 1;
             ensureCapa();
+            for (int i = size - 2; i >= index; i--) {
+                elements[i + 1] = elements[i];
+            }
+            elements[index] = e;
+            return true;
         }
-        elements[size++]=e;
+        return false;
     }
-    public E get (int i){
-        if(i>= size || i<0){
-            throw new IndexOutOfBoundsException("Index: "+ i + ", Size "+i);
+
+    public E get(int i) {
+        if (i >= 0 || i < size) {
+            return elements[i];
         }
-        return (E) elements[i];
+        return null;
+    }
+
+    public MyList clone() {
+        MyList<E> clone = new MyList<>(elements.length);
+        for (E x : elements) {
+            clone.add(x);
+        }
+        return clone;
+    }
+
+    public E[] getElements() {
+        return this.elements;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public boolean remove(int index) {
+        if (index >= 0 && index < size) {
+            for (int i = index; i < size; i++) {
+                elements[i] = elements[i + 1];
+            }
+            size -= 1;
+            return true;
+        }
+        return false;
+    }
+
+    public void clear() {
+        elements = (E[]) new Object[DEFAULT_CAPACITY];
+        size = 0;
+    }
+
+    public int indexOf(E e) {
+        for (int i = 0; i < size; i++) {
+            if (elements[i].equals(e)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public boolean contain(E e) {
+        for (E x : elements) {
+            if (e.equals(x)) {
+                return true;
+            }
+        }
+        return false;
     }
 }
--- module2/src/ss10_dsa_danh_sach/bai_tap/java_util_array_list/MyListTest.java
+++ module2/src/ss10_dsa_danh_sach/bai_tap/java_util_array_list/MyListTest.java
@@ -2,15 +2,33 @@ package ss10_dsa_danh_sach.bai_tap.java_util_array_list;
 
 public class MyListTest {
     public static void main(String[] args) {
-        MyList<Integer> listInteger = new MyList<Integer>();
-        listInteger.add(1);
-        listInteger.add(2);
-        listInteger.add(3);
-        listInteger.add(3);
-        listInteger.add(4);
-        System.out.println("element 4: " + listInteger.get(4));
-        System.out.println("element 1: " + listInteger.get(1));
-        System.out.println("element 2: " + listInteger.get(2));
+        MyList<String> list = new MyList<>();
+        list.add("A");
+        list.add("B");
+        list.add("C");
+        list.add("D");
+        list.add("E");
+        System.out.println(list.get(3));
+        System.out.println(list.get(8));
+        display(list.getElements());
+        System.out.println("element 4: " + list.get(4));
+        System.out.println("element 1: " + list.get(1));
+        System.out.println("element 3: " + list.get(2));
+        MyList<String> clone = list.clone();
+        System.out.println("Trong mảng có B không?");
+        System.out.println(clone.contain("B"));
+        System.out.println(clone.size());
+        list.remove(2);
+        System.out.println("Sau remove:");
+        display(list.getElements());
 
+    }
+
+    static void display(Object[] list) {
+        for (int i = 0; i < list.length; i++) {
+            if (list[i] != null) {
+                System.out.println("elment " + i + ": " + list[i]);
+            }
+        }
     }
 }
